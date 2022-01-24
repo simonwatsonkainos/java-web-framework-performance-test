9c02c73495a86f7d78dc79c5db2d1e96ecde96dc
Simon Watson <watsi88@outlook.com>
2022-01-24 20:28:56 +0000

    Deleting files which should be part of gitignore

--- dropwizardperformancetest/target/classes/db/migration/V1__Baseline.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE customer (
-    customer_id       MEDIUMINT NOT NULL AUTO_INCREMENT,
-    name              VARCHAR(255),
-    address           VARCHAR(255),
-    email             VARCHAR(255),
-    PRIMARY KEY (customer_id)
-);
-
-ALTER TABLE customer AUTO_INCREMENT = 1000;
